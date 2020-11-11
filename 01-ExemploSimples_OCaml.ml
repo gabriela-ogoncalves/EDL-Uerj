07c437039ed7a0f2780c28b52d73af0f82e5b6d3
Gabriela Gonçalves <68876780+gabriela-ogoncalves@users.noreply.github.com>
2020-11-11 19:06:33 -0300

    add exemplo simples e exemplo real em OCaml

---
+++ 01-ExemploSimples_OCaml.ml
@@ -0,0 +1,25 @@
+(* 
+   Programa simples escrito em OCaml, exemplificando as funções polimórficas padrões da biblioteca.
+   List.length --> recebe uma lista de qualquer tipo e retorna o seu tamanho.
+   List.hd --> recebe uma lista de qualquer tipo e retorna o primeiro elemento de cada uma. 
+*)
+
+List.length["ana";"joão";"josé"];; 
+List.length[false; true];;
+List.length[2; 4; 6; 8; 10];;
+(* 
+   Nessa primeira parte, temos os respectivos retornos:  
+  - : int = 3
+  - : int = 2
+  - : int = 5
+*)
+
+List.hd["ana";"joão";"josé"];;
+List.hd[false; true];;
+List.hd[2; 4; 6; 8; 10];;
+(* 
+   Nessa segunda parte, temos os respectivos retornos:  
+  - : string = "ana"
+  - : bool = false
+  - : int = 2
+*)
